66e19b071d33254176320765e00f65f9cd56bdca
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-02-03 11:58:41 -0300

    Imagens

--- mobile/Projeto1/App.js
+++ mobile/Projeto1/App.js
@@ -4,11 +4,22 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 class App extends Component{
   render(){
+    let nome = 'André Barreto'
+    let img = 'https://conteudo.imguol.com.br/c/esporte/8e/2021/09/11/depois-de-12-anos-cristiano-ronaldo-voltou-a-usar-a-camisa-do-manchester-united-1631369946867_v2_900x506.jpg.webp'
     return(
       <View>
-        <Text>Olá Mundo!!</Text>
+        <Text style={{color:'blue'}}>     </Text>
+        <Text style={{color:'blue'}}>Olá Mundo!!!!!!</Text>
         <Text style={{color:'blue', margin:10}}>Meu primeiro App!</Text>
+        <Image 
+        source={{uri: img}}
+        style={{width:300, height:300}}
+        />
+        <Text>Seja Bem vindo, {nome}</Text>
+        
+        
       </View>
+
     );
    
     
