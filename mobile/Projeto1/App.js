e58a6a284a1fddc047acdace3b08f6706bbb1898
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-02-04 17:06:38 -0300

    display

--- mobile/Projeto1/App.js
+++ mobile/Projeto1/App.js
@@ -1,29 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { ImageBackground } from 'react-native-web';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 class App extends Component{
-  render(){
-    let nome = 'André Barreto'
-    let img = 'https://conteudo.imguol.com.br/c/esporte/8e/2021/09/11/depois-de-12-anos-cristiano-ronaldo-voltou-a-usar-a-camisa-do-manchester-united-1631369946867_v2_900x506.jpg.webp'
-    return(
-      <View>
-        <Text style={{color:'blue'}}>     </Text>
-        <Text style={{color:'blue'}}>Olá Mundo!!!!!!</Text>
-        <Text style={{color:'blue', margin:10}}>Meu primeiro App!</Text>
-        <Image 
-        source={{uri: img}}
-        style={{width:300, height:300}}
-        />
-        <Text>Seja Bem vindo, {nome}</Text>
-        
-        
-      </View>
 
-    );
+  render(){
    
     
+    return(
+      <View style={{flex:1, backgroundColor:'#222'}}>
+         <View style={{height:65, backgroundColor:'red'}}></View>
+         <View style={{flex:1, backgroundColor:'#FFF'}}></View>
+         <View style={{height:65, backgroundColor:'red'}}></View>
+      </View>
+    );
   }
 }
 
 export default App;
+
+
+
+
